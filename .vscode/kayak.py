3f26d7564b36bd1bf49b53719a91063199abd2e3
Christina Passafaro <christinapassafaro@pop-os.localdomain>
2023-09-14 10:04:37 -0400

    setting up individual website scrapes

--- .vscode/kayak.py
+++ .vscode/kayak.py
@@ -6,24 +6,31 @@ boatlist = []
 headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
 
-def getBoats(page):
-    url = 'https://www.nextadventure.net/shop/paddle/kayaks/whitewater-kayaks?p={page}'
+def getBoats():
+    # gettign boats for next adventure site
+    allBoats = getBoatsFromNextAdventure(4)
+    return allBoats
+   
 
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    boats = soup.find_all('div', {'class': 'product-item-details'})
+def getBoatsFromNextAdventure(pages):
+    for x in range(0, pages):
+        # print(x)
+        url = 'https://www.nextadventure.net/shop/paddle/kayaks/whitewater-kayaks?p={page}'
 
-    for item in boats:
-        print(item)
-        boat = {
-        'title': item.find('a', {'class': 'product-item-link'}).text,
-        'link': item.find('a', {'class': 'product-item-link'})['href'],
-        'price': item.find('div', {'class': 'price-final_price'}).text if item.find('div', {'class': 'price-final_price'}) else '',
-        }
-        boatlist.append(boat)
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        boats = soup.find_all('div', {'class': 'product-item-details'})
+
+        for item in boats:
+            print(item)
+            boat = {
+            'title': item.find('a', {'class': 'product-item-link'}).text,
+            'link': item.find('a', {'class': 'product-item-link'})['href'],
+            'price': item.find('div', {'class': 'price-final_price'}).text if item.find('div', {'class': 'price-final_price'}) else '',
+            }
+            boatlist.append(boat)
     return
-
 # for()
-getBoats(2)
+getBoats()
 print(boatlist)
 
--- README.md
+++ README.md
@@ -3,3 +3,5 @@ scrape the internet for best deals on whitewater kayaks
 test
 
 to run in the terminal you must have python installed and run python3 then directory to kayak.py
+
+need to install nltk
