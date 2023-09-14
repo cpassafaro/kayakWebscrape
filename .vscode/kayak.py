e9e452b2d4bc0cecd9b0cd971c9565beb1927cbe
Christina Passafaro <christinapassafaro@pop-os.localdomain>
2023-09-14 14:50:25 -0400

    working through colorado kayak supply

--- .vscode/kayak.py
+++ .vscode/kayak.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 boatlist = [] 
-
+tester = []
 headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
 
@@ -14,23 +14,38 @@ def getBoats():
 
 def getBoatsFromNextAdventure(pages):
     for x in range(0, pages):
-        # print(x)
         url = 'https://www.nextadventure.net/shop/paddle/kayaks/whitewater-kayaks?p={page}'
 
         r = requests.get(url, headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
-        boats = soup.find_all('div', {'class': 'product-item-details'})
+        boats = soup.find_all('div', {'class': 'product-item-info'})
 
         for item in boats:
-            print(item)
+            img = item.find('img', {'class': 'product-image-photo'})
             boat = {
             'title': item.find('a', {'class': 'product-item-link'}).text,
             'link': item.find('a', {'class': 'product-item-link'})['href'],
             'price': item.find('div', {'class': 'price-final_price'}).text if item.find('div', {'class': 'price-final_price'}) else '',
+            'image': img['data-src'] if img['data-src'] else img['src'] 
             }
             boatlist.append(boat)
     return
-# for()
-getBoats()
-print(boatlist)
+
+def getBoatsFromColoradoKayak():
+    url = 'https://coloradokayak.com/collections/whitewater-kayaks'
+
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    mainboatarea = soup.find_all('div', {'class': 'card'})
+    for pages in mainboatarea:
+        header = pages.find('h1', {'class': 'heading'})
+        # only shop all whitewater kayak is set up this way so we don't need more detailed statement
+        if header:
+            boats = pages.find('div', {'class': 'product-item'})
+            print(boats)
+    return
+
+getBoatsFromColoradoKayak()
+# getBoats()
+# print(boatlist)
 
